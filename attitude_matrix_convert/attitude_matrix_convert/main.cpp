4de896a9002c56987e8b956a1388dcdac6a88396
GimHuang <807597370@qq.com>
2020-11-11 09:48:48 +0800

    基本完成，但需要检查bug

--- attitude_matrix_convert/attitude_matrix_convert/main.cpp
+++ attitude_matrix_convert/attitude_matrix_convert/main.cpp
@@ -5,16 +5,37 @@ using namespace std;
 using namespace Eigen;
 void main()
 {
-	//Vector3f value=Eigen::Vector3f::Zero();
-	//cout << value << endl;
-	Euler_angle ea1(1,2,3);
-	ea1.show();
-	Direct_cosine_matrix dcm(ea1.EA2DCM());
+	// test 
+	Vector3f initional_data;
+	initional_data << 1, 2, 3;
+	//Euler Angle
+	Euler_angle ea(initional_data);
+	cout << "## Euler angle ##" << endl;
+	ea.show();
+	cout << "## Euler 2 Direct cosine matrix ##" << endl;
+	Direct_cosine_matrix dcm(ea.EA2DCM());
 	dcm.show();
-	//dcm.show();
-	//*dcm.show();
-	//Direct_cosine_matrix dcm;
-	//dcm.value(1,1) = 1;
-	//dcm.show();
+	cout << "## Eulor 2 Quaternion ##" << endl;
+	Quaternion_vector qv(ea.EA2QV());
+	qv.show();
+
+	cout << endl << "## Direct cosine matrix ##" << endl;
+	dcm.show();
+	cout << "## Direct cosine matrix 2 Euler angle ##" << endl;
+	ea << dcm.DCM2EA();
+	ea.show();
+	cout << "## Direct cosine matrix 2 Quaternion ##" << endl;
+	qv << dcm.DCM2QV();
+	qv.show();
+
+	cout << endl << "## Quaternion vector ##" << endl;
+	qv.show();
+	cout << "## Quaternion 2 Euler angle ##" << endl;
+	ea << qv.QV2EA();
+	ea.show();
+	cout << "## Quaternion 2 Direct cosine matrix ##" << endl;
+	dcm << qv.QV2DCM();
+	dcm.show();
+
 	system("pause");
 }
